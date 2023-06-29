f7377a69f4bdc0ada550673887fff2e6e8304860
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-06-29 14:17:20 -0300

    RM

--- rick_and_morty/Server/src/index.js
+++ rick_and_morty/Server/src/index.js
@@ -1,6 +1,7 @@
 const server = require('./app');
 const PORT = 3001;
+const {conn} = require ('./DB_connection');
 
-
+conn.sync({force:true});
 
 server.listen(PORT, ()=>{console.log('Server up in port: ' + PORT);});
